7edfb0a3fa17811f8c7f0c0de38babf8d79e9966
Aamnah Akram <hello@aamnah.com>
2015-10-28 20:16:21 +0500

    v0.2

---
+++ README.md
@@ -0,0 +1,28 @@
+Changelog
+---
+
+### v0.1
+- basic code taken from a js fiddle demo
+
+### v0.2
+- went through the code and added comments
+- added variable `time` to get the time in a format which is easier to check in conditional statement
+- added if statements to check for zones based on current time
+
+### v0.3
+- added zone titles
+
+### v0.4
+- added zone quotes
+- refined CSS
+
+### v0.5
+- added variable keys for all the if statements to check zones
+
+
+Future
+---
+
+- add a to-do list (which changes based on the current zone, _obviously_)
+- add suggestions for what to do when the zone is upon me
+- show tasks/events/calendar entries with the relevnat tags. e.g. when in personal zone, click to show a list of relevant items that can be done during personal time.
--- script.js
+++ script.js
@@ -1,17 +1,58 @@
 (function () {
     function checkTime(i) {
-        return (i < 10) ? "0" + i : i;
+        return (i < 10) ? "0" + i : i;  // add a zero in front of numbers<10
     }
 
+    // Time
     function startTime() {
-        var today = new Date(),
-            h = checkTime(today.getHours()),
-            m = checkTime(today.getMinutes()),
-            s = checkTime(today.getSeconds());
-        document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-        t = setTimeout(function () {
+        var today = new Date(), // get current date and time
+            h = checkTime(today.getHours()), // get hour
+            m = checkTime(today.getMinutes()), // get minute
+            s = checkTime(today.getSeconds()); // get second
+            time = h.toString() + m.toString(); // get time in string form like 2123 for 9:23PM so that it's easier to use in if statements
+        
+        document.getElementById('time').innerHTML = h + ":" + m + ":" + s; // display time 12:34:56
+
+
+        // Change colors based on time zone
+        // Entertainment
+        if((time >= 1000 && time < 1030) || (time >= 1200 && time < 1300) || (time >= 1800 && time < 1900)) {
+        document.body.className = "entertainment";
+        } 
+        // Family
+        if(zones.family) {
+        document.body.className = "family";
+        }
+        // Learn
+        if((time >= 1300 && time < 1400) || (time >= 2130 && time < 2200)) {
+        document.body.className = "learn";
+        } 
+        // Personal
+        if((time >= 0600 && time < 0630) || (time >= 1500 && time < 1600) || (time >= 2200 && time < 2230)) {
+        document.body.className = "personal";
+        } 
+        // Sleep
+        if(time >= 2300) { // if it's 11PM or later, i'm asleep
+        document.body.className = "sleep";
+        }
+        // Solitude
+        if((time >= 0630 && time < 0700) || (time >= 2230 && time < 2300)) {
+        document.body.className = "solitude";
+        }
+        // Work
+        if((time >= 1030 && time < 1200) || (time >= 2100 && time < 2130)) {
+        document.body.className = "work";
+        }        
+        
+        // Refresh clock every minute
+        t = setTimeout(function () { // setTimeout() calls a function or executes a code after a specified delay
             startTime()
-        }, 500);
+        }, 1000); // 1m = 60000ms, 1s = 1000ms. settimeout() takes values in miliseconds
     }
     startTime();
 })();
+
+/* Sources
+**********
+http://www.plus2net.com/javascript_tutorial/clock.php
+http://jsfiddle.net/cse_tushar/fKKSb/311/
